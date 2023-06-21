1883e71b692fc2dcfb480e69f6ab69b640740a97
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-06-21 08:08:52 +0800

    added logs

--- pages/api/postmark-webhook.ts
+++ pages/api/postmark-webhook.ts
@@ -9,40 +9,18 @@ type Data = {
 };
 
 type PostmarkEvent = {
+  RecordType: string;
+  ServerID: number;
+  MessageStream: string;
+  MessageID: string;
+  Recipient: string;
+  Tag: string;
+  DeliveredAt: string;
+  Details: string;
   Metadata: {
     example: string;
     example_2: string;
   };
-  RecordType: string;
-  FirstOpen: boolean;
-  Client: {
-    Name: string;
-    Company: string;
-    Family: string;
-  };
-  OS: {
-    Name: string;
-    Company: string;
-    Family: string;
-  };
-  Platform: string;
-  UserAgent: string;
-  ReadSeconds: number;
-  Geo: {
-    CountryISOCode: string;
-    Country: string;
-    RegionISOCode: string;
-    Region: string;
-    City: string;
-    Zip: string;
-    Coords: string;
-    IP: string;
-  };
-  MessageID: string;
-  MessageStream: string;
-  ReceivedAt: string;
-  Tag: string;
-  Recipient: string;
 };
 
 export default async function handler(
@@ -51,29 +29,35 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     const emailEvent: PostmarkEvent = req.body;
-    console.log(emailEvent);
+    console.log("Received POST request:", emailEvent);
 
     var client = new postmark.ServerClient(
       "47b1320e-2a0f-4533-91a9-a2c065475c50"
     );
 
     try {
+      console.log("Attempting to send email...");
       await client.sendEmail({
         From: "support@getboon.ai",
-        To: "tms.vivaldi@gmail.com",
+        To: emailEvent.Recipient,
         Subject: "Hello from Postmark",
         HtmlBody: "<strong>Hello</strong> dear Postmark user.",
         TextBody: "Hello from Postmark!",
         MessageStream: "outbound",
       });
 
+      console.log("Email sent successfully!");
+
       // Send the response when the email is successfully sent
       res.status(200).json({ status: "Received" });
     } catch (error) {
+      console.error("Failed to send email:", error);
+
       // Send a 500 - Internal Server Error response if sending the email fails
       res.status(500).json({ error: "Failed to send email" });
     }
   } else {
+    console.log("Received non-POST request");
     res.status(405).json({ error: "Method not allowed" });
   }
 }
